2ed922376ff1b04581488c4436ca4749b2d6e500
Mylo L <100321671+MSLigg@users.noreply.github.com>
2022-05-31 20:13:13 -0400

    finished map

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const liveServer = require('live-server')
 
 async function main() {
 
-    app.listen(3001, () => {
+    app.listen(8081, () => {
         liveServer.start({
-            port: 3000,
+            port: 8081,
             logLevel: 0,
             root: './public'
         })
